bfd86f5c23985413452fddadc4390fd6240c43ab
Melanie <ruohongwei.wrh@gmail.com>
2026-07-14 16:09:24 +0100

    switch

---
+++ CapitalCities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conditionals
+{
+    public enum CapitalCities
+    {
+        London,
+        Paris,
+        Rome,
+        Madrid
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,10 +16,44 @@
             //}
             //Console.WriteLine($"The animal that lives in the {pole} Pole is the {animal}");
 
-            Pole pole = Pole.South;
-            string animal;
-            animal = (pole == Pole.North) ? "Polar bear" : "Penguin";
-            Console.WriteLine($"The animal that lives in the {pole} Pole is the {animal}");
+            //Pole pole = Pole.South;
+            //string animal;
+            //animal = (pole == Pole.North) ? "Polar bear" : "Penguin";
+            //Console.WriteLine($"The animal that lives in the {pole} Pole is the {animal}");
+            Console.WriteLine("Switch Statement");
+            var city = CapitalCities.Madrid;
+            string countryMessage = "";
+
+
+            switch (city)
+            {
+                case CapitalCities.London:
+                    countryMessage = $"{city} is capital of UK";
+                    Console.WriteLine(countryMessage);
+                    break;
+                case CapitalCities.Madrid:
+                    countryMessage = $"{city} is capital of Spain";
+                    Console.WriteLine(countryMessage);
+                    break;
+                case CapitalCities.Rome:
+                    countryMessage = $"{city} is capital of Italy";
+                    Console.WriteLine(countryMessage);
+                    break;
+                case CapitalCities.Paris:
+                    countryMessage = $"{city} is capital of France";
+                    Console.WriteLine(countryMessage);
+                    break;
+                default:
+                    Console.WriteLine(countryMessage);
+                    break;
+
+
+
+
+
+
+
+            }
         }
     }
 }
