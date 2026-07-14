6732c482f3de4824c79b5d6c5d5787a3172585f4
Liang <Leon.tang67@gmail.com>
2026-07-14 15:47:09 +0100

    Add if-else statement and ternary realisation

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Conditionals
+{
+    class Condits
+    {
+        static void Main(string[] args)
+        {
+            //Pole pole = Pole.North;
+            //string animal;
+            //if (pole == Pole.North)
+            //{
+            //    animal = "Polar bear";
+            //}
+            //else
+            //{
+            //    animal = "Penguin";
+            //}
+            //Console.WriteLine($"The animal that lives in the {pole} Pole is the {animal}");
+
+            Pole pole = Pole.South;
+            string animal;
+            animal = (pole == Pole.North) ? "Polar bear" : "Penguin";
+            Console.WriteLine($"The animal that lives in the {pole} Pole is the {animal}");
+        }
+    }
+}
+
